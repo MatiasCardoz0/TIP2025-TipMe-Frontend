8fae0586e3aec3e1a60fa59f0f5e661285ec7155
camila-am <marchinicamila94@gmail.com>
2025-05-26 22:38:47 -0300

    Arreglo lógica de redirección a home

--- mobile/app/MainLayout.tsx
+++ mobile/app/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Slot, useRouter, usePathname } from "expo-router";
 import React, { useEffect } from "react";
-import { useAuth, AuthProvider } from "../src/context/AuthContext";
+import { useAuth } from "../src/context/AuthContext";
 import Navbar from "./components/NavBar";
 
 export default function MainLayout() {
@@ -13,16 +13,14 @@ export default function MainLayout() {
   useEffect(() => {
       console.log("authState in useEffect:", authState, "pathname:", pathname);
 
-  if (authState?.authenticated === true) {
+  if (authState?.authenticated === true && pathname === "/") {
     router.replace("/home");
   } else if (authState?.authenticated === false) {
     router.replace("/login");
   }
   }, [authState, pathname]);
 
-  //uso stack para navegar múltiples páginas. 
-  // Expo router maneja la navegación entre pantallas, evita tener que usar react navigation y poner manualmente las rutas (<Stack.Screen name="homeScreen" component={HomeScreen} />, .etc)
-  return (
+   return (
     <>
         <Navbar />
         <Slot />
