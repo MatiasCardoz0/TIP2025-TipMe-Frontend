c9860668fce6f942aa916647a2dd8687af106773
camila-am <marchinicamila94@gmail.com>
2025-06-10 23:18:04 -0300

    Agrego test unitario

--- mobile/tests/home.test.tsx
+++ mobile/tests/home.test.tsx
@@ -4,19 +4,19 @@ import HomeScreen from "../app/home";
 
 describe("Modal de creación de mesa", () => {
   it("si los campos no estan completos se deshabilita el botón Confirmar", () => {
-    const { getByPlaceholderText, getByText } = render(<HomeScreen />);
+    const { getByPlaceholderText, getByText, getByTestId } = render(<HomeScreen />);
     fireEvent.press(getByText("╋ Agregar Mesa"));
 
     // traigo los elementos del modal
     const nombre = getByPlaceholderText("Nombre de la mesa");
     const numero = getByPlaceholderText("Numero de mesa");
-    const confirmarBtn = getByText("Confirmar");
+    const confirmarBtn = getByTestId("confirmar-btn");
 
     // solo el campo de 'nombre' completo
     fireEvent.changeText(nombre, "Mesa 1");
     expect(confirmarBtn.props.accessibilityState.disabled).toBe(true);
 
-    // solo el campo de 'numero' completo
+    // // solo el campo de 'numero' completo
     fireEvent.changeText(nombre, "");
     fireEvent.changeText(numero, "5");
     expect(confirmarBtn.props.accessibilityState.disabled).toBe(true);
@@ -31,4 +31,5 @@ describe("Modal de creación de mesa", () => {
     fireEvent.changeText(numero, "5.5");
     expect(confirmarBtn.props.accessibilityState.disabled).toBe(true);
   });
+
 });
