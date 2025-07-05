444b9081db00c90fe633fc8d3b1c642488de8ff7
camila-am <marchinicamila94@gmail.com>
2025-07-04 22:22:51 -0300

    Arreglamos la página de success

--- web-clients/src/Success.jsx
+++ web-clients/src/Success.jsx
@@ -3,31 +3,33 @@ import './components/Modal/TipModal.jsx'
 import logo from '../../shared/TipMe_Logo_transparent.png'
 import { config } from "../../config.js";
 import { useEffect } from "react";
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 function Success() {
     const navigate = useNavigate();
-  //   const [searchParams] = useSearchParams();
-  //   const mesaId = searchParams.get("id");
+    const [searchParams] = useSearchParams();
+    const externalRef = searchParams.get('external_reference');
+    const mesaId = externalRef ? externalRef.split('|')[0] : null;
+    const amount = externalRef ? externalRef.split('|')[1] : null;
+    
+    useEffect(() => {
+    if (mesaId) {
+      saveTip(mesaId);
+    }
+  }, [mesaId]);
 
-  //   useEffect(() => {
-  //   if (mesaId) {
-  //     //saveTip(mesaId);
-  //   }
-  // }, [mesaId]);
-
-  //   const saveTip = async (mesaId) => {           
-  //       try {
-  //           const response = await axios.post(config.API_URL+"/api/propina/grabar", {
-  //               "monto": amount,
-  //               "fecha": new Date().toISOString(),
-  //               "idMesa": mesaId,
-  //               //"idMozo": 1 
-  //       });
-  //       }
-  //       catch (error){
-  //         console.log("Error en el servidor." + error);
-  //         }
-  //     }
+    const saveTip = async (mesaId) => {           
+        try {
+            const response = await axios.post(config.API_URL+"/api/propina/grabar", {
+                "monto": amount,
+                "fecha": new Date().toISOString(),
+                "idMesa": mesaId,
+        });
+        }
+        catch (error){
+          console.log("Error en el servidor." + error);
+          }
+      }
 
   return (
     <div>
@@ -40,7 +42,7 @@ function Success() {
         <h2 className='portal-title'>Gracias por su colaboración!</h2>
 
         <div className='buttons'>
-          <button className='menu-button' onClick={() => navigate("/")}>
+          <button className='menu-button' onClick={() => navigate("/mesa/" + {mesaId})}>
             Volver
           </button>
         </div>
